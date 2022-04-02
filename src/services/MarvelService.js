23225cbf1c2bf4bfb713ba0a4ec79b609fb3aa4c
Yuliya Barieva <yuliya.zainab@gmail.com>
2022-04-02 13:43:43 +0200

    char list, error boundary, servise

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@
 class MarvelService {
     _apiBase = "https://gateway.marvel.com:443/v1/public/"
     _apiKey = "apikey=7c67a6b60b807bd80d6fd18235f24798"
+    
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -26,11 +27,13 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description  ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items // ? `${char.item.name.slice(0, 10)}...` : "Not comics",
         }
  
     }
